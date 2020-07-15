5f1785e4d79e4ef0251fada8f27b8ddfb3200788
Laith Tbaileh <nano.rlsh@gmail.com>
2020-07-15 15:45:10 +0900

    The background has been modified to fit with 1920x1080 resolution. activity_main.xml has the design but should be changed later in the development when the structure of the activities been decided.

--- EmoTechs_Andriod_App/app/src/main/java/com/example/emotechs/MainActivity.java
+++ EmoTechs_Andriod_App/app/src/main/java/com/example/emotechs/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.emotechs;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        /*This portion will make the Status bar and the Navigation bar transparent*/
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            Window w = getWindow();
+            w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        }
+        /*end of Transparency function*/
+
+
     }
 }
